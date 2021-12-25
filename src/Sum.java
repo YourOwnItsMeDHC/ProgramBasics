0e5f346b464d7f5d77cc3c92cb634fe967a5a460
YourOwnItsMeDHC <2019c08@sigce.edu.in>
2021-12-25 12:52:24 +0530

    sum of two numbers

--- src/Sum.java
+++ src/Sum.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
 
 public class Sum{
     public static void main(String[] args) {
-        System.out.println("Hello Deepak");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number which you want to add :");
+        int num1 = input.nextInt();
+        System.out.println("Your first number is :   " + num1);
+
+        System.out.print("Enter second number which you want to add : ");
+        int num2 = input.nextInt();
+        System.out.println("Your second number is :   " + num2);
+
+        int sum = num1 + num2;
+        System.out.println("Sum of two number is :" + sum);
+
+
     }
 }
 
