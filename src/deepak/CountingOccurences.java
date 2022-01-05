2a860fdcd3d1f218cc027478341b5696b1213155
YourOwnItsMeDHC <2019c08@sigce.edu.in>
2022-01-05 17:43:09 +0530

    Count Occurence or Repetition of number entered

--- src/deepak/CountingOccurences.java
+++ src/deepak/CountingOccurences.java
@@ -18,6 +18,7 @@ public class CountingOccurences {
         If we find remainder (%10) by 10 of any number , we get last digit of that number
         because we have numbers from 0 to 9 only which is not divisible by 10
         Eg : 456754 % 10 gives remainder as 4
+        If we get 4 == identify , then make count ++ , otherwise come out of "if" loop
 
         To remove last digit , divide that same number by 10 , so that last digit will get shift to decimal part
         , system will not consider it , because we haven't take flaot data type .
