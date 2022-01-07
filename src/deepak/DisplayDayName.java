7296dd572069e1a3082c35e42746db751b8719ee
YourOwnItsMeDHC <2019c08@sigce.edu.in>
2022-01-07 15:40:27 +0530

    Display Day name between 1 to 7 (SWITCH CASE)

---
+++ src/deepak/DisplayDayName.java
@@ -0,0 +1,72 @@
+package deepak;
+
+import java.util.Scanner;
+
+public class DisplayDayName {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter day number of day whose name you wanna know :  ");
+        byte day = input.nextByte();
+
+        switch(day) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("Enter day number between 1 to 7");
+        }
+
+//        //traditional switch statement
+//        switch (day) {
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            case 4:
+//                System.out.println("Thursday");
+//                break;
+//            case 5:
+//                System.out.println("Friday");
+//                break;
+//            case 6:
+//                System.out.println("Saturday");
+//                break;
+//            case 7:
+//                System.out.println("Sunday");
+//                break;
+//            default:
+//                System.out.println("Enter day number between 1 to 7");
+//                break;
+//        }
+
+//        //using "if"
+//        if (day == 1) {
+//            System.out.println("Monday");
+//        } else if (day == 2) {
+//            System.out.println("Tuesday");
+//        } else if (day == 3) {
+//            System.out.println("Wednesday");
+//        } else if (day == 4) {
+//            System.out.println("Thursday");
+//        } else if (day == 5) {
+//            System.out.println("Friday");
+//        } else if (day == 6) {
+//            System.out.println("Saturday");
+//        } else if (day == 7) {
+//            System.out.println("Sunday");
+//        } else {
+//            System.out.println("Enter day number between 1 to 7");
+//        }
+
+
+    }
+}
